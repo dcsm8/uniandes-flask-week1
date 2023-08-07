a3f9fca372303fe5e34fa40fb535e3533904b52e
David Sánchez <dc.sanchezm1@uniandes.edu.co>
2023-08-07 18:16:23 -0500

    feat: complete task1, task2, task3

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,4 +3,6 @@ from flask import Flask
 
 def create_app(config_name):
     app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tutorial_canciones.sqlite"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     return app
--- flaskr/app.py
+++ flaskr/app.py
@@ -1,5 +1,11 @@
 from flaskr import create_app
+from .modelos import db
 
 app = create_app("default")
 app_context = app.app_context()
 app_context.push()
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
