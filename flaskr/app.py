60f496aed6ac5f5703488fdaaad49e4aabe712b7
David Sánchez <dc.sanchezm1@uniandes.edu.co>
2023-08-07 19:07:15 -0500

    feat: add rest endpoints

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,11 +1,31 @@
 from flaskr import create_app
+from flask_restful import Api
+
+from flaskr.vistas.vistas import VistaUserManagement
 from .modelos import db
+from .vistas import (
+    VistaCanciones,
+    VistaCancion,
+    VistaSignIn,
+    VistaLogIn,
+    VistaAlbum,
+    VistaAlbumsUsuario,
+    VistaCancionesAlbum,
+)
 
 app = create_app("default")
 app_context = app.app_context()
 app_context.push()
 
 db.init_app(app)
+db.create_all()
 
-with app.app_context():
-    db.create_all()
+api = Api(app)
+api.add_resource(VistaCanciones, "/canciones")
+api.add_resource(VistaCancion, "/cancion/<int:id_cancion>")
+api.add_resource(VistaSignIn, "/signin")
+api.add_resource(VistaUserManagement, "/user/<int:id_usuario>")
+api.add_resource(VistaLogIn, "/login")
+api.add_resource(VistaAlbumsUsuario, "/usuario/<int:id_usuario>/albumes")
+api.add_resource(VistaAlbum, "/album/<int:id_album>")
+api.add_resource(VistaCancionesAlbum, "/album/<int:id_album>/canciones")
